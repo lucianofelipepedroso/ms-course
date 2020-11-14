e2c54edb167ff513e5dd215759938897fd69db1d
Luciano <luciano.pedroso@live.com>
2020-11-14 17:50:11 -0300

    Project hr-worker create

---
+++ hr-worker/src/test/java/com/luciano/felipe/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luciano.felipe.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
