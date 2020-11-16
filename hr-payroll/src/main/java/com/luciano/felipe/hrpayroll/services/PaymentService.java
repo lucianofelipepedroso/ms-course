7b7a049cdbbcce802d808e28426374a516f3df18
Luciano <luciano.pedroso@live.com>
2020-11-16 15:19:44 -0300

    FeignClients

--- hr-payroll/src/main/java/com/luciano/felipe/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/luciano/felipe/hrpayroll/services/PaymentService.java
@@ -1,29 +1,30 @@
 package com.luciano.felipe.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.luciano.felipe.hrpayroll.entities.Payment;
 import com.luciano.felipe.hrpayroll.entities.Worker;
+import com.luciano.felipe.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
-	@Autowired
-	private RestTemplate restTemplate;
+//	@Autowired
+//	private RestTemplate restTemplate;
 	
-	@Value(value = "${hr-worker.host}")
-	private String workerHost;
+	@Autowired
+	private WorkerFeignClient workerFeignClient;
+//	
+//	@Value(value = "${hr-worker.host}")
+//	private String workerHost;
 	
 	public Payment getPayment(long workerId, int days) {
-		Map<String,String> uriVariable = new HashMap<>();
-		uriVariable.put("id", String.valueOf(workerId));
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}",Worker.class,uriVariable);
+//		Map<String,String> uriVariable = new HashMap<>();
+//		uriVariable.put("id", String.valueOf(workerId));
+//		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}",Worker.class,uriVariable);
+		Worker worker = workerFeignClient.findById(workerId).getBody();
+		
 		return new Payment(worker.getName(),worker.getDailyIncome(), days);
 	}
 
