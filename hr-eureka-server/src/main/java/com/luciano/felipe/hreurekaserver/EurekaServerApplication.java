a4ff2ebcb03f696f8153681a7849ebdc68708a1b
Luciano <luciano.pedroso@live.com>
2020-11-17 09:34:53 -0300

    Configuring Eureka Server

--- hr-eureka-server/src/main/java/com/luciano/felipe/hreurekaserver/EurekaServerApplication.java
+++ hr-eureka-server/src/main/java/com/luciano/felipe/hreurekaserver/EurekaServerApplication.java
@@ -2,7 +2,11 @@ package com.luciano.felipe.hreurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
