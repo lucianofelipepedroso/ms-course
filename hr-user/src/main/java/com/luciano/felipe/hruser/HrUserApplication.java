73236d450dd23bdb85349b15b63cbc3245ae7512
Luciano <luciano.pedroso@live.com>
2020-11-18 17:41:58 -0300

    Configuring h-user project

--- hr-user/src/main/java/com/luciano/felipe/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/luciano/felipe/hruser/HrUserApplication.java
@@ -2,11 +2,15 @@ package com.luciano.felipe.hruser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+@EnableEurekaClient
 @SpringBootApplication
 public class HrUserApplication {
 
 	public static void main(String[] args) {
+	
 		SpringApplication.run(HrUserApplication.class, args);
 	}
 
