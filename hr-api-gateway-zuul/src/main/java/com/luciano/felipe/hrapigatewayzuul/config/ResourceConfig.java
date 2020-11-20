789bae06cbb030a3703abf7069f89ff8a640112d
Luciano <luciano.pedroso@live.com>
2020-11-20 18:02:39 -0300

    Zuul authorization

---
+++ hr-api-gateway-zuul/src/main/java/com/luciano/felipe/hrapigatewayzuul/config/ResourceConfig.java
@@ -0,0 +1,40 @@
+package com.luciano.felipe.hrapigatewayzuul.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableResourceServer
+public class ResourceConfig extends ResourceServerConfigurerAdapter {
+	
+	private static final String[] PUBLIC = {"/hr-oauth/oauth/token"};
+	
+	private static final String[] OPERATOR = {"/hr-worker/**"};
+	
+	private static final String[] ADMIN = {"/hr-payroll/**","/hr-user/**"};
+
+	@Autowired
+	private JwtTokenStore jwtTokenStore;
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers(PUBLIC).permitAll()
+		.antMatchers(HttpMethod.GET,OPERATOR).hasAnyRole("OPERATOR","ADMIN")
+		.antMatchers(ADMIN).hasRole("ADMIN")
+		.anyRequest().authenticated();
+
+	}
+
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.tokenStore(jwtTokenStore);
+	}
+
+}
