d4a4fb86f96336085bdbbb7994eb385ef795461e
Luciano <luciano.pedroso@live.com>
2020-11-20 16:22:29 -0300

    Login,JWT, Token

--- hr-oauth/src/main/java/com/luciano/felipe/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/luciano/felipe/hroauth/services/UserService.java
@@ -3,13 +3,16 @@ package com.luciano.felipe.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.luciano.felipe.hroauth.entities.User;
 import com.luciano.felipe.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService{
 	
 	@Autowired
 	private UserFeignClient userFeignClient;
@@ -24,6 +27,18 @@ public class UserService {
 			throw new IllegalArgumentException("E-mail not found");
 		}
 			logger.info("E-mail found: " + email);
+		return user;
+	}
+
+
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(userName).getBody();
+		if(user==null) {
+			logger.error("E-mail not found: " + userName);
+			throw new UsernameNotFoundException ("E-mail not found");
+		}
+			logger.info("E-mail found: " + userName);
 		return user;
 	}
 
